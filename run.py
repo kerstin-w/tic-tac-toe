a3ca19e9c619d49daf9fb069f0b78758ec0ac1f6
Kerstin <kerstinweibeler@gmail.com>
2023-01-30 06:45:23 +0000

    Feat: Add function to display the game board grid

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Board:
+    """Represents the board for the Tic-Tac-Toe game"""
+
+    def __init__(self):
+        """Initializes a new board."""
+        self.board = [[i + j for i in range(1, 4)] for j in range(0, 9, 3)]
+
+    def display_board(self):
+        """
+        Create the grid for the game
+        """
+        print("--------+-------+--------")
+        for row in self.board:
+            print("|       |       |       |")
+            print("|  ", row[0], "  |  ", row[1], "  |  ", row[2], "  | ")
+            print("|       |       |       |")
+            print("--------+-------+--------")
+
+
+if __name__ == '__main__':
+    Board().display_board()
