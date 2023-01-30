e6e1c44e048fd3935b477bb986344a1c95990680
Kerstin <kerstinweibeler@gmail.com>
2023-01-30 08:01:41 +0000

    Feat: Add function to ask user ether to play against computer or human

--- run.py
+++ run.py
@@ -17,5 +17,34 @@ class Board:
             print("--------+-------+--------")
 
 
-if __name__ == '__main__':
-    Board().display_board()
+class Game:
+    """Represents a Tic-Tac-Toe game."""
+
+    def __init__(self):
+        """Initilize 2 Players and one Board."""
+        self.player1 = "X"
+        self.player2 = "O"
+        self.board = Board()
+        self.is_computer_player = False
+
+    def choose_player(self):
+        """
+        Ask user if the game is going to be played against the computer,
+        or against a second human player
+        """
+        player = ""
+        print("Would you like to play against a friend or the computer?\n")
+        while True:
+            player = input("Enter computer or human: ")
+            if player not in {"computer", "human"}:
+                print("\nOopsi! Wrong entry.")
+                continue
+            break
+        if player.lower() == "computer":
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    Game().choose_player()
